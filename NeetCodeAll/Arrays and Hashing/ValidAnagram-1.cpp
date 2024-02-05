8956a775cc3960f892895667305e7e9099e7712d
Sam Lu <pbcmarsh@gmail.com>
2024-02-05 11:27:04 -0600

    Create ValidAnagram-1.cpp

---
+++ NeetCodeAll/Arrays and Hashing/ValidAnagram-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (size(s) != size(t)) {
+            return false;
+        }
+        unordered_map<char, int> counts;
+        for (char x : s) {
+            counts[x]++;
+        }
+        for (char x : t) {
+            counts[x]--;
+        }
+        for (auto count : counts) {
+            if (count.second != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+/*
+Strategy: Unordered map counts
+n is the max length of strings s and t
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
