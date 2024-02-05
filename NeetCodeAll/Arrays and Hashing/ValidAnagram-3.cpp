9662d4158387b2ed6eac3e91954460082281e246
Sam Lu <pbcmarsh@gmail.com>
2024-02-05 11:33:50 -0600

    Create ValidAnagram-3.cpp

---
+++ NeetCodeAll/Arrays and Hashing/ValidAnagram-3.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int counts[26];
+        for (char c : s) {
+            counts[c - 'a']++;
+        }
+        for (char c : t) {
+            counts[c - 'a']--;
+        }
+        for (int count : counts) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+/*
+Strategy: Hash Table with array
+n is the max length of strings s and t
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
